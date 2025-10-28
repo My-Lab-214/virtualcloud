912605f46f0c7c11a46e472d7144f3651badabdc
My-Lab-214 <111333766+My-Lab-214@users.noreply.github.com>
2025-10-28 20:37:35 +0530

    Remove password2 from SignUpForm fields

--- cloudapp/forms.py
+++ cloudapp/forms.py
@@ -6,7 +6,7 @@ from .models import UserFile
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'password1']
 
 class FileUploadForm(forms.ModelForm):
     class Meta:
